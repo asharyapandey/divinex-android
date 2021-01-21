36acfd1417d05f1a5cd1f60d3e401dc42d5387ce
asharyapandey <asharyap7@gmail.com>
2021-01-21 17:31:10 +0545

    added validation function for login activity

--- app/src/main/java/com/asharya/divinex/LoginActivity.kt
+++ app/src/main/java/com/asharya/divinex/LoginActivity.kt
@@ -3,18 +3,48 @@ package com.asharya.divinex
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var tvRegister : TextView
+    private lateinit var etUsername : EditText
+    private lateinit var etPassword : EditText
+    private lateinit var btnLogin : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
         tvRegister = findViewById(R.id.tvRegister)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+        }
 
         tvRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    private fun validate() : Boolean {
+        when {
+            TextUtils.isEmpty(etUsername.text) -> {
+                etUsername.error = "Please Enter Your Username"
+                etUsername.requestFocus()
+                return false
+            }
+            TextUtils.isEmpty(etPassword.text) -> {
+                etPassword.error = "Please Enter Your Username"
+                etPassword.requestFocus()
+                return false
+            }
+            return true
         }
     }
 }
