49e550f020cb4ef99bc46413f252de06bdf321c6
asharyapandey <asharyap7@gmail.com>
2021-01-21 18:09:43 +0545

    added fragments for dashboard

---
+++ app/src/main/java/com/asharya/divinex/fragments/ProfileFragment.kt
@@ -0,0 +1,10 @@
+package com.asharya.divinex.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.asharya.divinex.R
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
