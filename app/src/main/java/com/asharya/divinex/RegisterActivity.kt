a448d9069832c28634e65cec1e05cdc07d80d31d
asharyapandey <asharyap7@gmail.com>
2021-01-28 17:03:04 +0545

    binded views in register activity

--- app/src/main/java/com/asharya/divinex/RegisterActivity.kt
+++ app/src/main/java/com/asharya/divinex/RegisterActivity.kt
@@ -2,10 +2,27 @@ package com.asharya.divinex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var etUsername: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnRegister: Button
+    private lateinit var rgGender : RadioGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword= findViewById(R.id.etPassword)
+        etConfirmPassword= findViewById(R.id.etConfirmPassword)
+        etEmail= findViewById(R.id.etEmail)
+        btnRegister = findViewById(R.id.btnRegister)
+        rgGender = findViewById(R.id.rgGender)
+
     }
 }
