48cf80e819cd122e4ca600e2da7198534882f7f4
asharyapandey <asharyap7@gmail.com>
2021-01-28 16:42:11 +0545

    added function in the dao to add user

--- app/src/main/java/com/asharya/divinex/dao/UserDAO.kt
+++ app/src/main/java/com/asharya/divinex/dao/UserDAO.kt
@@ -1,4 +1,10 @@
 package com.asharya.divinex.dao
 
+import androidx.room.Insert
+import com.asharya.divinex.model.User
+
 interface UserDAO {
+
+    @Insert
+    suspend fun insertUser(user: User)
 }
