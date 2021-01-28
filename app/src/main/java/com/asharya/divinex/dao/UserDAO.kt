ae475f41e6dd93b9a70aa936d44cf11d99e0fe47
asharyapandey <asharyap7@gmail.com>
2021-01-28 16:48:29 +0545

    added function in the dao to retrieve user for login

--- app/src/main/java/com/asharya/divinex/dao/UserDAO.kt
+++ app/src/main/java/com/asharya/divinex/dao/UserDAO.kt
@@ -1,10 +1,15 @@
 package com.asharya.divinex.dao
 
 import androidx.room.Insert
+import androidx.room.Query
 import com.asharya.divinex.model.User
 
 interface UserDAO {
 
     @Insert
     suspend fun insertUser(user: User)
+
+
+    @Query("select * from User where username=(:username) and password=(:password)")
+    suspend fun retrieveUser(username: String, password: String) : User
 }
