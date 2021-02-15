149961dc30dd5f53860d821f330da80e50cd2b28
asharyapandey <asharyap7@gmail.com>
2021-02-15 19:54:50 +0545

    added registerUser function in user repository

---
+++ app/src/main/java/com/asharya/divinex/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.asharya.divinex.repository
+
+import com.asharya.divinex.api.ApiRequest
+import com.asharya.divinex.api.ServiceBuilder
+import com.asharya.divinex.api.UserAPI
+import com.asharya.divinex.model.User
+import com.asharya.divinex.response.LoginResponse
+
+class UserRepository: ApiRequest() {
+    // retrofit instance of userAPI
+    private val userApi = ServiceBuilder.buildService(UserAPI::class.java)
+
+    // register user
+    suspend fun registerUser(user: User) : LoginResponse {
+        return apiRequest {
+            userApi.registerUser(user)
+        }
+    }
+
+}
