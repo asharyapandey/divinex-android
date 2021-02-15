676ed0bb0c81a03390ee4eed7b2ae8d3eca17a9f
asharyapandey <asharyap7@gmail.com>
2021-02-15 19:12:35 +0545

    added function to login user in the userAPI interface

--- app/src/main/java/com/asharya/divinex/api/UserAPI.kt
+++ app/src/main/java/com/asharya/divinex/api/UserAPI.kt
@@ -1,15 +1,27 @@
 package com.asharya.divinex.api
 
 import com.asharya.divinex.model.User
+import com.asharya.divinex.response.LoginResponse
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface UserAPI {
 
     // Registering User
-    @POST("api/user")
+    @POST("user")
     suspend fun registerUser(
         @Body user: User
-    )
+    ) : Response<LoginResponse>
+
+    // Login
+    @FormUrlEncoded
+    @POST("user/login")
+    suspend fun loginUser(
+        @Field("username") username : String,
+        @Field("password") password : String
+    ) : Response<LoginResponse>
 
 }
