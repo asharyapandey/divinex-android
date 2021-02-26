bb5a68936c0f1e3498078d7e12a39863b0e92c52
asharyapandey <asharyap7@gmail.com>
2021-02-26 10:41:10 +0545

    added test for userlogin

--- app/src/main/java/com/asharya/divinex/api/ServiceBuilder.kt
+++ app/src/main/java/com/asharya/divinex/api/ServiceBuilder.kt
@@ -5,7 +5,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL = "http://10.0.2.2:5000/api/"
+//    private const val BASE_URL = "http://10.0.2.2:5000/api/"
+    private const val BASE_URL = "http://localhost:5000/api/"
+
+
 
     val token : String? = null
 
---
+++ app/src/test/java/com/asharya/divinex/PhotoSharingTest.kt
@@ -0,0 +1,19 @@
+package com.asharya.divinex
+
+import com.asharya.divinex.repository.UserRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+class PhotoSharingTest {
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun testLogin() = runBlocking {
+        userRepository = UserRepository()
+        val response = userRepository.loginUser("ash","wakandaforever")
+        val expectedResult = true
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+}
