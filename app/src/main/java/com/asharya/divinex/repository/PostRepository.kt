c8c6bbe94ad1028ba31ed92e8a84eb9db0cb1d71
asharyapandey <asharyap7@gmail.com>
2021-02-27 17:00:42 +0545

    added three methods in PostRepository

--- app/src/main/java/com/asharya/divinex/repository/PostRepository.kt
+++ app/src/main/java/com/asharya/divinex/repository/PostRepository.kt
@@ -5,10 +5,26 @@ import com.asharya.divinex.api.PostAPI
 import com.asharya.divinex.api.ServiceBuilder
 import com.asharya.divinex.model.Post
 import com.asharya.divinex.response.AddPostResponse
+import com.asharya.divinex.response.PostsResponse
 
 class PostRepository : ApiRequest() {
     private val postAPI = ServiceBuilder.buildService(PostAPI::class.java)
 
     // add post
-    suspend fun addPost(post: Post) : AddPostResponse
+    suspend fun addPost(post: Post) : AddPostResponse {
+        return apiRequest {
+            postAPI.addPost(ServiceBuilder.token!!, post)
+        }
+    }
+    suspend fun getPostFeed() : PostsResponse {
+        return apiRequest {
+            postAPI.getPostFeed(ServiceBuilder.token!!)
+        }
+    }
+
+    suspend fun getUserPosts() : PostsResponse {
+        return apiRequest {
+            postAPI.getUsersPost(ServiceBuilder.token!!)
+        }
+    }
 }
