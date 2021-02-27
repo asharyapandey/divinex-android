cf1f84a748ef1a2521a73186ff915ccbec791b96
asharyapandey <asharyap7@gmail.com>
2021-02-27 17:33:53 +0545

    added functions to show user selected image in imageviwe

--- app/src/main/java/com/asharya/divinex/ui/fragments/addpost/AddPostFragment.kt
+++ app/src/main/java/com/asharya/divinex/ui/fragments/addpost/AddPostFragment.kt
@@ -1,16 +1,32 @@
 package com.asharya.divinex.ui.fragments.addpost
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.asharya.divinex.R
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 class AddPostFragment : Fragment() {
     private lateinit var viewModel: AddPostViewModel
+    private lateinit var ivPostImage: ImageView
+    private lateinit var etPostCaption : EditText
+    private lateinit var btnUploadPost: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,14 +38,90 @@ class AddPostFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_post, container, false)
 
-        val ivPostImage: ImageView = view.findViewById(R.id.ivPostImage)
-        val etPostCaption: EditText = view.findViewById(R.id.etPostCaption)
-        val btnUploadPost : Button = view.findViewById(R.id.btnUploadPost)
+        ivPostImage = view.findViewById(R.id.ivPostImage)
+        etPostCaption= view.findViewById(R.id.etPostCaption)
+        btnUploadPost= view.findViewById(R.id.btnUploadPost)
 
         ivPostImage.setOnClickListener {
-
+            loadPopUpMenu()
         }
         return view
+    }
+
+    private fun loadPopUpMenu() {
+        val popupMenu = PopupMenu(context,ivPostImage)
+        popupMenu.menuInflater.inflate(R.menu.gallery_camera, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+            when(item.itemId) {
+                R.id.menuCamera -> openCamera()
+                R.id.menuGallery -> openGallery()
+            }
+            true
+        }
+    }
+
+    private var REQUEST_GALLERY_CODE = 0
+    private var REQUEST_CAMERA_CODE = 1
+    private var imageUrl: String? = null
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY_CODE)
+    }
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CAMERA_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_GALLERY_CODE && data != null) {
+                val selectedImage = data.data
+                val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                val contentResolver = context?.contentResolver
+                val cursor =
+                    contentResolver?.query(selectedImage!!, filePathColumn, null, null, null)
+                cursor!!.moveToFirst()
+                val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+                imageUrl = cursor.getString(columnIndex)
+                ivPostImage.setImageBitmap(BitmapFactory.decodeFile(imageUrl))
+                cursor.close()
+            } else if (requestCode == REQUEST_CAMERA_CODE && data != null) {
+                val imageBitmap = data.extras?.get("data") as Bitmap
+                val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val file = bitmapToFile(imageBitmap, "$timeStamp.jpg")
+                imageUrl = file!!.absolutePath
+                ivPostImage.setImageBitmap(BitmapFactory.decodeFile(imageUrl))
+            }
+        }
+
+    }
+    private fun bitmapToFile(
+        bitmap: Bitmap,
+        fileNameToSave: String
+    ): File? {
+        var file: File? = null
+        return try {
+            file = File(
+                context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                    .toString() + File.separator + fileNameToSave
+            )
+            file.createNewFile()
+            //Convert bitmap to byte array
+            val bos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos) // YOU can also save it in JPEG
+            val bitMapData = bos.toByteArray()
+            //write the bytes in file
+            val fos = FileOutputStream(file)
+            fos.write(bitMapData)
+            fos.flush()
+            fos.close()
+            file
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            file // it will return null
+        }
     }
 
 }
