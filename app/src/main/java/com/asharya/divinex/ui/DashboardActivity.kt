749f0a15a29ae85fd3320b9ace7820d54e0edcae
asharyapandey <asharyap7@gmail.com>
2021-02-27 17:15:29 +0545

    added permissions

--- app/src/main/java/com/asharya/divinex/ui/DashboardActivity.kt
+++ app/src/main/java/com/asharya/divinex/ui/DashboardActivity.kt
@@ -1,8 +1,10 @@
 package com.asharya.divinex.ui
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.asharya.divinex.R
 import com.asharya.divinex.api.ServiceBuilder
@@ -14,7 +16,13 @@ import com.asharya.divinex.ui.fragments.search.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
-    private lateinit var bnvDashboard : BottomNavigationView
+    private lateinit var bnvDashboard: BottomNavigationView
+    private val permissions = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -22,16 +30,17 @@ class DashboardActivity : AppCompatActivity() {
         bnvDashboard = findViewById(R.id.bnvDashboard)
 
         val homeFragment = HomeFragment()
-        val notificationFragment= NotificationFragment()
+        val notificationFragment = NotificationFragment()
         val addPhotoFragment = AddPostFragment()
         val searchFragment = SearchFragment()
         val profileFragment = ProfileFragment()
 
         setFragment(homeFragment)
+        checkRunTimePermission()
         Toast.makeText(this, "${ServiceBuilder.token}", Toast.LENGTH_SHORT).show()
 
         bnvDashboard.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.miHome -> setFragment(homeFragment)
                 R.id.miProfile -> setFragment(profileFragment)
                 R.id.miNotifiation -> setFragment(notificationFragment)
@@ -44,10 +53,35 @@ class DashboardActivity : AppCompatActivity() {
 
     }
 
-    private fun setFragment(fragment : Fragment) =
+    private fun setFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.flFragment, fragment)
             addToBackStack(null)
             commit()
         }
+
+    private fun checkRunTimePermission() {
+        if (!hasPermission()) {
+            requestPermission()
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+                break
+            }
+        }
+        return hasPermission
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this@DashboardActivity, permissions, 1)
+    }
 }
--- app/src/main/java/com/asharya/divinex/ui/fragments/addpost/AddPostFragment.kt
+++ app/src/main/java/com/asharya/divinex/ui/fragments/addpost/AddPostFragment.kt
@@ -1,6 +1,35 @@
 package com.asharya.divinex.ui.fragments.addpost
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.asharya.divinex.R
-class AddPostFragment : Fragment(R.layout.fragment_add_post) {
+class AddPostFragment : Fragment() {
+    private lateinit var viewModel: AddPostViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_post, container, false)
+
+        val ivPostImage: ImageView = view.findViewById(R.id.ivPostImage)
+        val etPostCaption: EditText = view.findViewById(R.id.etPostCaption)
+        val btnUploadPost : Button = view.findViewById(R.id.btnUploadPost)
+
+        ivPostImage.setOnClickListener {
+
+        }
+        return view
+    }
+
 }
