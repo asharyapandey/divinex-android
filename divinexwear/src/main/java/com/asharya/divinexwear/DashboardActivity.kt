71ba0c3658444810d6768a358c3acc028592b492
asharyapandey <asharyap7@gmail.com>
2021-04-15 14:13:17 +0545

    finished wear os

--- divinexwear/src/main/java/com/asharya/divinexwear/DashboardActivity.kt
+++ divinexwear/src/main/java/com/asharya/divinexwear/DashboardActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.widget.TextView
 import com.asharya.divinex.api.ServiceBuilder
+import com.bumptech.glide.Glide
 import de.hdodenhof.circleimageview.CircleImageView
 import org.w3c.dom.Text
 
@@ -26,6 +27,14 @@ class DashboardActivity : WearableActivity() {
         tvFollowers= findViewById(R.id.tvFollowers)
         tvFollowing= findViewById(R.id.tvFollowinge)
         civProfile = findViewById(R.id.civProfile)
-        tvUsername.text = ServiceBuilder.currentUser?.username
+        tvUsername.text = "Hello, " + ServiceBuilder.currentUser?.username
+        tvEmail.text = ServiceBuilder.currentUser?.email
+        tvFollowing.text = ServiceBuilder.currentUser?.following?.size.toString() + " Following"
+        tvFollowers.text = ServiceBuilder.currentUser?.followers?.size.toString() + " Followers"
+
+
+        var profImagePath = ServiceBuilder.loadImagePath() + ServiceBuilder.currentUser?.profilePicture
+        profImagePath = profImagePath.replace("\\", "/")
+        Glide.with(this).load(profImagePath).into(civProfile)
     }
 }
