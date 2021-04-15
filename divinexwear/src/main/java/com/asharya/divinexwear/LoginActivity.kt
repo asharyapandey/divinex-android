a6fc5588b82e5ae2f3cbb756da66189acef67f57
asharyapandey <asharyap7@gmail.com>
2021-04-15 13:41:22 +0545

    added layout for login page

--- divinexwear/src/main/java/com/asharya/divinexwear/LoginActivity.kt
+++ divinexwear/src/main/java/com/asharya/divinexwear/LoginActivity.kt
@@ -2,8 +2,14 @@ package com.asharya.divinexwear
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : WearableActivity() {
+    private lateinit var etUsername: TextInputEditText
+    private lateinit var etPassword: TextInputEditText
+    private lateinit var btnLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,5 +17,16 @@ class LoginActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword= findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password= etPassword.text.toString().trim()
+
+            Toast.makeText(this, "$username, $password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ divinexwear/src/main/java/com/asharya/divinexwear/api/ServiceBuilder.kt
@@ -0,0 +1,38 @@
+package com.asharya.divinex.api
+
+import com.asharya.divinex.model.User
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+//    private const val BASE_URL = "http://10.0.2.2:5000/api/"
+    private const val BASE_URL = "http://192.168.1.66:5000/api/"
+
+    var token : String? = null
+    var currentUser: User? = null
+
+    // logger
+    private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttp = OkHttpClient.Builder().addInterceptor(logger)
+
+    private val retrofitBuilder = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp.build())
+
+    // retrofit instance
+    private val retrofit = retrofitBuilder.build()
+
+    // generic function
+    fun <T> buildService(serviceType: Class<T>) : T {
+        return retrofit.create(serviceType)
+    }
+
+    fun loadImagePath(): String {
+        val arr = BASE_URL.split("/").toTypedArray()
+        return arr[0] + "/" + arr[1] + arr[2] + "/"
+    }
+}
